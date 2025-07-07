abbbc04d80780d3462c4fba4c4088a5c446f2b13
IsaacNgRWI <isaacngrwi@gmail.com>
2025-07-07 22:25:21 +0800

    Added function to load token from .env file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 
 client = commands.Bot(command_prefix = '!', intents=discord.Intents.default())  # sets up the command prefix
 

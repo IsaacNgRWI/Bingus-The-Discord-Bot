37e4d7ccbf1fcd366be8f9c5d56035a8cbd65a51
IsaacNgRWI <isaacngrwi@gmail.com>
2025-07-07 22:33:18 +0800

    Outlined privileged gateway intents for the bot

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
+intents = discord.Intents.default
+intents.message_content = True
+intents.members = True
+intents.presences = True
+
 client = commands.Bot(command_prefix = '!', intents=discord.Intents.default())  # sets up the command prefix
 
 @client.event
